beebb0f837d76e77c06d5f89c9c03b1cdf464d95
north-redwing <narita9226@gmail.com>
2021-08-14 17:17:18 +0900

    add vim zsh tmux

---
+++ dotfilesLink.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+ln -sf ~/dotfiles/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
